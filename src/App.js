2b4de43b2c555db49e45c0d38168480b0f9fce85
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-07-25 11:53:52 +0530

    Used in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./redux/counter";
+
 const App = () => {
-  return <div>Redux</div>;
+  const { value } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <center>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
+      <code>{value}</code>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(incrementByAmount(value));
+        }}
+      >
+        Double It
+      </button>
+    </center>
+  );
 };
 
 export default App;
