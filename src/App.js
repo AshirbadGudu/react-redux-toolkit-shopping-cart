72695f0b7440e3d5c7f3a5f002f9370805b3ae05
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-07-25 13:24:25 +0530

    ProductCard component created and used

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { Button, Card, Col, Divider, PageHeader, Row } from "antd";
-import Layout, { Content, Footer } from "antd/lib/layout/layout";
-import { useDispatch, useSelector } from "react-redux";
-import { addToCart, removeFromCart } from "./redux/shop";
 import "antd/dist/antd.css";
+import { Divider, PageHeader, Row } from "antd";
+import Layout, { Content, Footer } from "antd/lib/layout/layout";
+import { useSelector } from "react-redux";
+import { ProductCard } from "./components";
 const App = () => {
   const { cartItems, products } = useSelector((state) => state.shop);
-  const dispatch = useDispatch();
-  const isInCart = (id) => {
-    const item = cartItems.find((item) => item.id === id);
-    return item;
-  };
+
   return (
     <Layout>
       <PageHeader
@@ -22,46 +18,7 @@ const App = () => {
         <Divider orientation="left">Products</Divider>
         <Row align="middle" justify="center">
           {products.map((product) => (
-            <Col lg={5} md={12} sm={12} key={`${product.id}`}>
-              <Card
-                title={product.title}
-                hoverable
-                style={{ width: 240, margin: "1vh 0" }}
-                cover={
-                  <img
-                    src={product.image}
-                    alt=""
-                    style={{
-                      width: "100%",
-                      height: 240,
-                    }}
-                  />
-                }
-                actions={[
-                  isInCart(product.id) ? (
-                    <Button
-                      danger
-                      type="primary"
-                      onClick={() => dispatch(removeFromCart(product.id))}
-                    >
-                      Remove From Cart
-                    </Button>
-                  ) : (
-                    <Button
-                      type="primary"
-                      onClick={() => dispatch(addToCart(product))}
-                    >
-                      Add To Cart
-                    </Button>
-                  ),
-                ]}
-              >
-                <Card.Meta
-                  title={product.price}
-                  description={product.category}
-                />
-              </Card>
-            </Col>
+            <ProductCard product={product} key={product.id} />
           ))}
         </Row>
       </Content>
@@ -69,37 +26,7 @@ const App = () => {
       <Footer>
         <Row align="middle" justify="center">
           {cartItems.map((product) => (
-            <Col lg={5} md={12} sm={12} key={`${product.id}`}>
-              <Card
-                title={product.title}
-                hoverable
-                style={{ width: 240, margin: "1vh 0" }}
-                cover={
-                  <img
-                    src={product.image}
-                    alt=""
-                    style={{
-                      width: "100%",
-                      height: 240,
-                    }}
-                  />
-                }
-                actions={[
-                  <Button
-                    danger
-                    type="primary"
-                    onClick={() => dispatch(removeFromCart(product.id))}
-                  >
-                    Remove From Cart
-                  </Button>,
-                ]}
-              >
-                <Card.Meta
-                  title={product.price}
-                  description={product.category}
-                />
-              </Card>
-            </Col>
+            <ProductCard product={product} key={product.id} />
           ))}
         </Row>
       </Footer>
---
+++ src/components/ProductCard.js
@@ -0,0 +1,55 @@
+import { Button, Card, Col } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../redux/shop";
+
+const ProductCard = ({ product }) => {
+  const { cartItems } = useSelector((state) => state.shop);
+  const dispatch = useDispatch();
+  const isInCart = (id) => {
+    const item = cartItems.find((item) => item.id === id);
+    return item;
+  };
+  return (
+    <>
+      <Col lg={5} md={12} sm={12} key={`${product.id}`}>
+        <Card
+          title={product.title}
+          hoverable
+          style={{ width: 240, margin: "1vh 0" }}
+          cover={
+            <img
+              src={product.image}
+              alt=""
+              style={{
+                width: "100%",
+                height: 240,
+              }}
+            />
+          }
+          actions={[
+            isInCart(product.id) ? (
+              <Button
+                danger
+                type="primary"
+                onClick={() => dispatch(removeFromCart(product.id))}
+              >
+                Remove From Cart
+              </Button>
+            ) : (
+              <Button
+                type="primary"
+                onClick={() => dispatch(addToCart(product))}
+              >
+                Add To Cart
+              </Button>
+            ),
+          ]}
+        >
+          <Card.Meta title={product.price} description={product.category} />
+        </Card>
+      </Col>
+    </>
+  );
+};
+
+export default ProductCard;
