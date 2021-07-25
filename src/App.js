ef36b1709e76b242b3fcec9fd969cb65c3174108
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-07-25 12:36:09 +0530

    shoping cart functionality added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
+import { Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment, incrementByAmount } from "./redux/counter";
+import { addToCart, removeFromCart } from "./redux/shop";
 
 const App = () => {
-  const { value } = useSelector((state) => state.counter);
+  const { cartItems, products } = useSelector((state) => state.shop);
   const dispatch = useDispatch();
+  const isInCart = (id) => {
+    const item = cartItems.find((item) => item.id === id);
+    return item;
+  };
   return (
-    <center>
-      <button
-        onClick={() => {
-          dispatch(decrement());
-        }}
-      >
-        Decrement
-      </button>
-      <code>{value}</code>
-      <button
-        onClick={() => {
-          dispatch(increment());
-        }}
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => {
-          dispatch(incrementByAmount(value));
-        }}
-      >
-        Double It
-      </button>
-    </center>
+    <main>
+      {products.map((product) => (
+        <Fragment key={`${product.id}`}>
+          <img
+            src={product.image}
+            alt=""
+            style={{
+              width: 50,
+              height: 50,
+            }}
+          />
+          <h4>{product.title}</h4>
+          {isInCart(product.id) ? (
+            <button onClick={() => dispatch(removeFromCart(product.id))}>
+              removeFromCart
+            </button>
+          ) : (
+            <button onClick={() => dispatch(addToCart(product))}>
+              addToCart
+            </button>
+          )}
+        </Fragment>
+      ))}
+      {cartItems.map((product) => (
+        <Fragment key={`${product.id}`}>
+          <img
+            src={product.image}
+            alt=""
+            style={{
+              width: 50,
+              height: 50,
+            }}
+          />
+          <h4>{product.title}</h4>
+          <button onClick={() => dispatch(removeFromCart(product.id))}>
+            removeFromCart
+          </button>
+        </Fragment>
+      ))}
+    </main>
   );
 };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counter from "./counter";
+import shop from "./shop";
 
 export const store = configureStore({
   reducer: {
     counter,
+    shop,
   },
 });
