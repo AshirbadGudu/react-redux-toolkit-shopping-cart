eae4c3181a623577e16167054caae33093d01655
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-10-09 07:46:48 +0530

    loader show durring data fetch

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,20 +1,21 @@
-import { Divider, Row, Skeleton } from "antd";
-import { useSelector } from "react-redux";
+import { Spin, Divider, Row } from "antd";
 import { AppHeader, ProductCard } from "../components";
 import { useGetProductsQuery } from "../redux/api";
 const Products = () => {
-  const { products } = useSelector((state) => state.shop);
-  const { data, error, isLoading } = useGetProductsQuery();
+  const { data, isLoading } = useGetProductsQuery();
 
   return (
     <>
       <AppHeader />
       <Divider orientation="left">Products</Divider>
       <Row align="middle" justify="center">
-        {!isLoading &&
+        {isLoading ? (
+          <Spin size="large" />
+        ) : (
           data.map((product) => (
             <ProductCard product={product} key={product.id} />
-          ))}
+          ))
+        )}
       </Row>
     </>
   );
